d891a6d1f730dc739c5c069ac4fe60332fbe401e
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2023-04-07 21:18:22 -0700

    chore revert docker build

--- src/ingress-nginx-validate-jwt-tests/EndToEndBase.cs
+++ src/ingress-nginx-validate-jwt-tests/EndToEndBase.cs
@@ -9,25 +9,10 @@ namespace ingress_nginx_validate_jwt_tests
     {
         readonly IContainer container;
 
-        readonly string ImageName = $"ingress-nginx-validate-jwt-tests:{Guid.NewGuid()}";
-
         public EndToEndBase()
         {
-            string fullPath = Path.GetFullPath("../../../../ingress-nginx-validate-jwt");
-            var stdErrBuffer = new StringBuilder();
-
-            var result = Cli.Wrap("docker")
-                .WithArguments(new[] { "build", "-t", ImageName, "-f", "Dockerfile", "." })
-                .WithWorkingDirectory(fullPath)
-                .WithStandardErrorPipe(PipeTarget.ToStringBuilder(stdErrBuffer))
-                .WithValidation(CommandResultValidation.None)
-                .ExecuteAsync().GetAwaiter().GetResult();
-
-            Console.WriteLine(stdErrBuffer);
-
             container = new ContainerBuilder()
-              //.WithImage("ingress-nginx-validate-jwt")
-              .WithImage(ImageName)
+              .WithImage("ingress-nginx-validate-jwt")
               .WithEnvironment("OpenIdProviderConfigurationUrl", "https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration")
               .WithPortBinding(8080)
               .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(8080))
@@ -39,12 +24,6 @@ namespace ingress_nginx_validate_jwt_tests
         public void Dispose()
         {
             container.StopAsync().Wait();
-
-            Thread.Sleep(TimeSpan.FromSeconds(10));
-
-            Cli.Wrap("docker")
-                .WithArguments(new[] { "image", "rm", ImageName })
-                .ExecuteAsync().GetAwaiter().GetResult();
         }
     }
 }
