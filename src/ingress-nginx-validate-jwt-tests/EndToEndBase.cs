3292fbea1c13cd79f9ac1051f3d050edf72a5ee8
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2023-04-07 18:56:07 -0700

    chore: build container in test

--- src/ingress-nginx-validate-jwt-tests/EndToEndBase.cs
+++ src/ingress-nginx-validate-jwt-tests/EndToEndBase.cs
@@ -1,28 +1,39 @@
 ﻿using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Containers;
+using DotNet.Testcontainers.Images;
 
 namespace ingress_nginx_validate_jwt_tests
 {
     public class EndToEndBase : IDisposable
     {
-        TestcontainersContainer testcontainersContainer;
+        readonly IContainer container;
+
+        readonly IFutureDockerImage image;
 
         public EndToEndBase()
         {
-            var testcontainersBuilder = new TestcontainersBuilder<TestcontainersContainer>()
-              .WithImage("ivanjosipovic/ingress-nginx-validate-jwt")
-              .WithName("ingress-nginx-validate-jwt")
+            image = new ImageFromDockerfileBuilder()
+                .WithDockerfileDirectory("../../../../ingress-nginx-validate-jwt")
+                .WithName($"ingress-nginx-validate-jwt-tests:{Guid.NewGuid()}")
+                .Build();
+
+            image.CreateAsync().Wait();
+
+            container = new ContainerBuilder()
+              //.WithImage("ingress-nginx-validate-jwt")
+              .WithImage(image.FullName)
               .WithEnvironment("OpenIdProviderConfigurationUrl", "https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration")
               .WithPortBinding(8080)
-              .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(8080));
+              .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(8080))
+              .Build();
 
-            testcontainersContainer = testcontainersBuilder.Build();
-            testcontainersContainer.StartAsync().Wait();
+            container.StartAsync().Wait();
         }
 
         public void Dispose()
         {
-            testcontainersContainer.StopAsync().Wait();
+            container.StopAsync().Wait();
+            image.DeleteAsync().Wait();
         }
     }
 }
