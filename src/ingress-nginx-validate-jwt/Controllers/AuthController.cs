02ce5c59e9b79c4a10e4960b26426dccfa120a70
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2023-05-25 14:54:14 -0700

    chore: null check

--- src/ingress-nginx-validate-jwt/Controllers/AuthController.cs
+++ src/ingress-nginx-validate-jwt/Controllers/AuthController.cs
@@ -66,7 +66,7 @@ public class AuthController : ControllerBase
                         {
                             foreach (var value in item.Value)
                             {
-                                if (value == null)
+                                if (string.IsNullOrEmpty(value))
                                 {
                                     continue;
                                 }
