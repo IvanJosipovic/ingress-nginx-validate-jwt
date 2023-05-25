a0fa5350f6007c7d7d2c56ed8ee8f04d11c303b6
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2023-05-24 21:52:03 -0700

    chore: add test

--- src/ingress-nginx-validate-jwt-tests/SettingsServiceTest.cs
+++ src/ingress-nginx-validate-jwt-tests/SettingsServiceTest.cs
@@ -33,4 +33,28 @@ public class SettingServiceTest
 
         configRetriever.Verify(x => x.GetConfigurationAsync("https://demo", It.IsAny<HttpDocumentRetriever>(), It.IsAny<CancellationToken>()), Times.Once);
     }
+
+    [Fact]
+    public async Task TestNullConfig()
+    {
+        IServiceCollection services = new ServiceCollection();
+
+        var configRetriever = new Mock<IConfigurationRetriever<OpenIdConnectConfiguration>>();
+        configRetriever.Setup(c => c.GetConfigurationAsync(It.IsAny<string>(), It.IsAny<IDocumentRetriever>(), It.IsAny<CancellationToken>())).Verifiable();
+
+        var config = new ConfigurationBuilder().AddInMemoryCollection(new Dictionary<string, string>()).Build();
+        services.AddSingleton<IConfiguration>(config);
+        services.AddLogging();
+        services.AddSingleton<ILoggerFactory, NullLoggerFactory>();
+        services.AddSingleton(configRetriever.Object);
+        services.AddSingleton<ISettingsService, SettingsService>();
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        var settingsService = serviceProvider.GetRequiredService<ISettingsService>();
+
+        Func<Task> act = async () => await settingsService.GetConfiguration();
+
+        await act.Should().ThrowAsync<Exception>().WithMessage("Environment Variable OpenIdProviderConfigurationUrl is null!");
+    }
 }
